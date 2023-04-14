d0c4f907e1c47abd07a0abed63ce7621ed5f7806
Marius Gomes <info@mgomes.de>
2023-04-14 12:26:21 +0200

    Initial Release

---
+++ Termine-finden/README.md
@@ -0,0 +1,53 @@
+# Termine finden - Moodle Datenbank-Vorlage
+
+## Download
+
+Download der Vorlagen-Datei: [Vorlagensatz.zip](https://github.com/margomius/moodle-datenbanken-vorlagen/raw/main/Termine-finden/Vorlagensatz.zip)
+
+## Beschreibung
+
+Eine Vorlage für Moodle-Datenbanken, um Termine mit kleinen oder großen Gruppen zu finden.
+Trainer:innen können konfigurieren, welche Termine zur Auswahl stehen und Teilnehmer:innen geben präferierte Termine an.
+Die Datenbank hat viele Funktionen, den beliebtesten Termin der gesamten Gruppe oder einer Teilgruppe zu finden und die Ergebnisse zu teilen.
+
+## Video-Tutorials (YouTube)
+
+Datenbank erstellen und Vorlagensatz importieren: [Video] (#)
+"Termine finden" für Teilnehmer:innen: [Video](#)
+""Termine finden" für Trainer:innen: [Video](#)
+
+## Features
+
+* Es gibt für Trainer:innen einige Konfigurations-Möglichkeiten, für die keine HTML/CSS/JavaScript-Kenntnisse erforderlich sind
+* In der Datenbank können Erklärungen und ein Video-Tutorial eingeblendet werden
+* Sämtliche Seiten nutzen ein responsive Bootstrap Design
+* In der Listenansicht wird abhängig von Bildschirmbreite und Anzahl der Einträge entschieden, welche Art von Einträgen/Symbolen angezeigt wird.
+* Trainer:innen sehen einen Button-Generator, um die Datenbank als Button im Moodle-Kurs einzubinden (damit läd sie etwas schneller)
+
+## To-Do
+
+* Die Funktion überarbeiten, wie der Moodle Name in "Eintrag hinzufügen" ausgelesen wird
+* Die Farben intelligenter programmieren, sodass auch mehr als 30 verschiedene Farben möglich sind
+* Eine "Wenn-es-sein-muss"-Option hinzufügen
+
+## Versionsverlauf
+
+* 1.0
+    * Initial Release
+
+## Screenshots
+
+### Eintrag hinzufügen
+![image](https://github.com/margomius/moodle-datenbanken-vorlagen/blob/main/Termine-finden/Screenshot/01_Neuer_Eintrag.PNG?raw=true)
+
+### Einzelansicht
+![image](https://github.com/margomius/moodle-datenbanken-vorlagen/blob/main/Termine-finden/Screenshot/02_Einzelansicht.PNG?raw=true)
+
+### Listenansicht
+![image](https://github.com/margomius/moodle-datenbanken-vorlagen/blob/main/Termine-finden/Screenshot/03_Listenansicht.PNG?raw=true)
+
+### Termin-Zusammenfassung (Listenansicht)
+![image](https://github.com/margomius/moodle-datenbanken-vorlagen/blob/main/Termine-finden/Screenshot/04_Zusammenfassung.PNG?raw=true)
+
+### Optionen für Trainer:innen
+![image](https://github.com/margomius/moodle-datenbanken-vorlagen/blob/main/Termine-finden/Screenshot/05_Fuer_Trainer_innen.PNG?raw=true)
---
+++ Termine-finden/Vorlagensatz/jstemplate.js
@@ -0,0 +1,151 @@
+// ++++++++++ Anleitung ++++++++++
+//
+// Hier kann die Datenbank konfiguriert werden.
+// Einfach den jeweiligen Wert hinter dem "=" verändern.
+// Nach Änderungen muss ggf. mit [Strg + F5] aktualisiert werden!
+//
+// ++++++++++ EINSTELLUNGEN ++++++++++
+
+// ++++++++++ Moodle-Namen für Teilnehmer:innen erzwingen ++++++++++
+
+const MoodleName = false;               // false = Eigene Eingabe; true = Moodle-Name
+
+// ++++++++++ Video-Tutorial für Teilnehmer:innen ein-/ausblenden ++++++++++
+
+const VideoTutorial = false;            // false = ausgeblendet; true = eingeblendet
+
+// ++++++++++ Spalten ein-/ausblenden und Spalten-Überschriften eingeben ++++++++++
+
+const TextSpalte00 = "Stunde"           // Überschrift Spalte 0 (Kann nicht ausgeblendet werden)
+const AbkSpalte00 = "Std."              // Kurzer Text für schmale Bildschirme
+
+
+const AnzahlSpalten = 5                 // Anzahl der Spalten (1-7), die eingeblendet werden
+
+
+const TextSpalte01 = "Montag"           // Überschrift Spalte 1
+const AbkSpalte01 = "Mo"                // Kurzer Text für schmale Bildschirme
+
+const TextSpalte02 = "Dienstag"
+const AbkSpalte02 = "Di"
+
+const TextSpalte03 = "Mittwoch"
+const AbkSpalte03 = "Mi"
+
+const TextSpalte04 = "Donnerstag"
+const AbkSpalte04 = "Do"
+
+const TextSpalte05 = "Freitag"
+const AbkSpalte05 = "Fr"
+
+const TextSpalte06 = "Samstag"
+const AbkSpalte06 = "Sa"
+
+const TextSpalte07 = "Sonntag"
+const AbkSpalte07 = "So"
+
+// ++++++++++ X Zeilen ein-/ausblenden ++++++++++
+// "AnzahlZeilen" = 0, dann werden die Zeilen durch die Einstellungen unten ein-/ausgeblendet.
+// "AnzahlZeilen" = X, dann werden X Zeilen eingeblendet, unabhängig von den Einstellungen unten.
+
+const AnzahlZeilen = 0                  // Mögliche Anzahl: 1 - 17  |   0 = deaktiviert
+
+// ++++++++++ Zeilen individuell ein-/ausblenden und Zeilen-Texte eingeben ++++++++++
+// Um Zeilen einzeln ein-/auszublenden, muss die Zeile "AnzahlZeilen" mit "//" auskommentiert sein.
+
+let Zeile01Anzeigen = true;             // false = ausgeblendet true = eingeblendet
+const TextZeile01 = "1. Stunde"         // Fetter Text, der in der ersten Spalte der ersten Zeile steht
+const ZeitZeile01 = "07:45 - 08:30"     // Normaler Text, der darunter steht
+
+let Zeile02Anzeigen = true;
+const TextZeile02 = "2. Stunde"
+const ZeitZeile02 = "08:30 - 09:15"
+
+let Zeile03Anzeigen = true;
+const TextZeile03 = "1. Pause"
+const ZeitZeile03 = "09:15 - 09:35"
+
+let Zeile04Anzeigen = true;
+const TextZeile04 = "3. Stunde"
+const ZeitZeile04 = "09:35 - 10:20"
+
+let Zeile05Anzeigen = true;
+const TextZeile05 = "4. Stunde"
+const ZeitZeile05 = "10:20 - 11:05"
+
+let Zeile06Anzeigen = true;
+const TextZeile06 = "2. Pause"
+const ZeitZeile06 = "11:05 - 11:30"
+
+let Zeile07Anzeigen = true;
+const TextZeile07 = "5. Stunde"
+const ZeitZeile07 = "11:30 - 12:15"
+
+let Zeile08Anzeigen = true;
+const TextZeile08 = "6. Stunde"
+const ZeitZeile08 = "12:15 - 13:00"
+
+let Zeile09Anzeigen = true;
+const TextZeile09 = "Mittagspause"
+const ZeitZeile09 = "13:00 - 13:50"
+
+let Zeile10Anzeigen = true;
+const TextZeile10 = "7. Stunde"
+const ZeitZeile10 = "13:50 - 14:35"
+
+let Zeile11Anzeigen = true;
+const TextZeile11 = "8. Stunde"
+const ZeitZeile11 = "14:35 - 15:20"
+
+// ++++++++++ 5 weitere Einträge ein-/ausblenden (standardmäßig ausgeblendet) ++++++++++
+
+let Zeile12Anzeigen = false;            // false = ausgeblendet true = eingeblendet
+const TextZeile12 = "9. Stunde"         // Fetter Text, der in der ersten Spalte der 12. Zeile steht
+const ZeitZeile12 = "15:30 - 16:15"     // Text, der darunter steht
+
+let Zeile13Anzeigen = false;
+const TextZeile13 = "10. Stunde"
+const ZeitZeile13 = "16:15 - 17:00"
+
+let Zeile14Anzeigen = false;
+const TextZeile14 = "11. Stunde"
+const ZeitZeile14 = "17:00 - 17:45"
+
+let Zeile15Anzeigen = false;
+const TextZeile15 = "12. Stunde"
+const ZeitZeile15 = "17:45 - 18:30"
+
+let Zeile16Anzeigen = false;
+const TextZeile16 = "13. Stunde"
+const ZeitZeile16 = "18:30 - 19:15"
+
+let Zeile17Anzeigen = false;
+const TextZeile17 = "14. Stunde"
+const ZeitZeile17 = "19:15 - 20:00"
+
+// ++++++++++ Einzelne Zeilen dunkel hervorheben  ++++++++++
+
+const DunkleZeile01 = false             // false = Zeile nicht dunkler; true = Zeile dunkler
+const DunkleZeile02 = false
+const DunkleZeile03 = true
+const DunkleZeile04 = false
+const DunkleZeile05 = false
+const DunkleZeile06 = true
+const DunkleZeile07 = false
+const DunkleZeile08 = false
+const DunkleZeile09 = true
+const DunkleZeile10 = false
+const DunkleZeile11 = false
+const DunkleZeile12 = false
+const DunkleZeile13 = false
+const DunkleZeile14 = false
+const DunkleZeile15 = false
+const DunkleZeile16 = false
+const DunkleZeile17 = false
+
+
+// +++++++++++++++++ EINSTELLUNGEN ENDE +++++++++++++++++
+
+
+// Termine finden v1.0 - Copyright (C) 2023, @margomius - https://margomius.com/
+// GNU General Public Licence 3.0 - http://www.gnu.de/documents/gpl-3.0.en.html
